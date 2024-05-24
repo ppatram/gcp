f9dfff7d8742b4680a766f745662d710d097119a
root <root@gcp-controlloer.us-east1-d.c.intense-wares-422520-j0.internal>
2024-05-24 16:56:26 +0000

    server setup complete

--- sandbox/setup.sql
+++ sandbox/setup.sql
@@ -1,32 +1,25 @@
+drop database if exists gcp_usage;
 create database if not exists gcp_usage;
 use gcp_usage;
-CREATE TABLE IF NOT EXISTS gcp_usage(
+CREATE TABLE IF NOT EXISTS persons(
         id MEDIUMINT NOT NULL AUTO_INCREMENT,
         firstname varchar(20),
         lastname varchar(20),
         login varchar(20),
         accesscode varchar(12),
+	region varchar(20) not null default 'asia-south1-a',
+	serverip varchar(20),
 	primary key (id,login)
 );
-insert into gcp_usage values('AYAN', 'BHADURI', 'abhaduri', lpad(conv(floor(rand()*pow(36,12)), 10, 36), 12, 0));
-insert into gcp_usage values( 'MAYANK'  'MITTAL', 'mmittal', lpad(conv(floor(rand()*pow(36,12)), 10, 36), 12, 0));
-insert into gcp_usage values('N',  'PRAKASH', 'nprakash', lpad(conv(floor(rand()*pow(36,12)), 10, 36), 12, 0));
-insert into gcp_usage values('MOHAMMAD', 'KHAJA', 'mkhaja' lpad(conv(floor(rand()*pow(36,12)), 10, 36), 12, 0));
-insert into gcp_usage values('RAMAKRISHNA', 'YADAV', 'rhadav', lpad(conv(floor(rand()*pow(36,12)), 10, 36), 12, 0));
-insert into gcp_usage values('HARICHARAN', 'REDDY', 'hreddy', lpad(conv(floor(rand()*pow(36,12)), 10, 36), 12, 0));
-insert into gcp_usage values('AKHIL', 'NAIDU', 'anaidu', lpad(conv(floor(rand()*pow(36,12)), 10, 36), 12, 0));
-insert into gcp_usage values('KRISHNAJIT',  'KAR', 'kkar', lpad(conv(floor(rand()*pow(36,12)), 10, 36), 12, 0));
-insert into gcp_usage values('SIVA', 'PRASAD', 'sprasad', lpad(conv(floor(rand()*pow(36,12)), 10, 36), 12, 0));
-insert into gcp_usage values('MARLON', 'THOMAS', lpad(conv(floor(rand()*pow(36,12)), 10, 36), 12, 0));
+insert into persons values('','AYAN', 'BHADURI', 'abhaduri', lpad(conv(floor(rand()*pow(36,12)), 10, 36), 12, 0), 'asia-south1-a', '');
+insert into persons values('', 'MAYANK',  'MITTAL', 'mmittal', lpad(conv(floor(rand()*pow(36,12)), 10, 36), 12, 0), 'asia-south1-a', '');
+insert into persons values('','N',  'PRAKASH', 'nprakash', lpad(conv(floor(rand()*pow(36,12)), 10, 36), 12, 0), 'asia-south1-a', '');
+insert into persons values('','MOHAMMAD', 'KHAJA', 'mkhaja', lpad(conv(floor(rand()*pow(36,12)), 10, 36), 12, 0), 'asia-south1-a', '');
+insert into persons values('','RAMAKRISHNA', 'YADAV', 'ryadav', lpad(conv(floor(rand()*pow(36,12)), 10, 36), 12, 0), 'asia-south1-a', '');
+insert into persons values('','HARICHARAN', 'REDDY', 'hreddy', lpad(conv(floor(rand()*pow(36,12)), 10, 36), 12, 0), 'asia-south1-a', '');
+insert into persons values('','AKHIL', 'NAIDU', 'anaidu', lpad(conv(floor(rand()*pow(36,12)), 10, 36), 12, 0), 'asia-south1-a', '');
+insert into persons values('','KRISHNAJIT',  'KAR', 'kkar', lpad(conv(floor(rand()*pow(36,12)), 10, 36), 12, 0), 'asia-south1-a', '');
+insert into persons values('','SIVA', 'PRASAD', 'sprasad', lpad(conv(floor(rand()*pow(36,12)), 10, 36), 12, 0), 'asia-south1-a', '');
+insert into persons values('','MARLON', 'THOMAS', 'mthomas', lpad(conv(floor(rand()*pow(36,12)), 10, 36), 12, 0), 'us-east1-d', '');
 
 
-
-CREATE TABLE IF NOT EXISTS logins(
-        id MEDIUMINT NOT NULL AUTO_INCREMENT,
-        login varchar(20),
-        logintime timestamp not null default current_timestamp,
-        logouttime timestamp not null default current_timestamp,
-        status ENUM('notprov', 'stopped', 'running') default 'notprov',
-        serverip varchar(20)
-	primary key (id,login)
-);
---
+++ sandbox/www/getmyip.php
@@ -0,0 +1,23 @@
+<?php
+$login=$_POST["login"];
+
+$filename = "/var/www/html/tmp/$login";
+
+$myfile = fopen($filename, "r") or die("<H1>Did you start your server?</H1><a href=/index.php>click here</a>");
+$txt = fread($myfile,filesize($filename));
+fclose($myfile);
+$parts = explode(",", $txt);
+
+$ip = $parts[6];
+$login = $parts[3];
+
+if ($ip == "") {
+	echo "<h1>Your server is not started or not provisioned yet</h1>";
+}
+
+
+echo "Your server ip is $ip; Login name is $login";
+unlink($filename)
+
+
+?>
