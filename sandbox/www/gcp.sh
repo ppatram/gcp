47e1630cb52aea0597db01586e5e440db7533333
parm <ppatram@gmail.com>
2024-05-26 16:20:48 +0000

    commit

--- sandbox/www/gcp.sh
+++ sandbox/www/gcp.sh
@@ -12,25 +12,17 @@ tmpfile=$(mktemp /tmp/startup.XXXXX)
 cp /var/lib/docker/volumes/docker-lamp_php/_data/custom-startup.sh $tmpfile
 sed -i "s/__person__/$user/" $tmpfile
 
-# get vm ip
-thisip=$(gcloud compute instances describe $vmn --zone $zone |grep -i natip | cut -d: -f2)
-
-if [ "$thisip" ]; then
-	echo "Server already there, let's start it"
-	gcloud compute instances start $vmn --zone $zone
-else
-
-	gcloud compute addresses create ${ipn} --zone $zone --ip-version IPV4
-	thisip=$(gcloud compute addresses list | grep -E "^$ipn" | awk '{print $2}')
-	gcloud compute instances create "${vmn}" \
+gcloud compute addresses create ${ipn} --zone $zone --ip-version IPV4
+thisip=$(gcloud compute addresses list | grep -E "^$ipn" | awk '{print $2}')
+gcloud compute instances create "${vmn}" \
 		--zone "$zone" --address=$thisip --machine-type "n1-standard-2" \
 		--boot-disk-size "40G" --boot-disk-type "pd-ssd" \
 		--boot-disk-device-name "${diskn}" \
 		--metadata-from-file=startup-script=$tmpfile
-fi	
-sed -i "s/,$/,$thisip/" $datfile	
-cat $datfile | tr -d ' ' > ${datfile}${count}
-mv ${datfile}${count} $datfile
+
+thisip=$(gcloud compute instances describe $vmn --zone $zone |grep -i natip | cut -d: -f2 | tr -d ' ')
+
+sed -i "s/,$/,$thisip/"  $datfile	
 
 rm -f $tmpfile
 
