34dc0e0374de3bbee1a3cbe5df6d4231784ec80a
root <root@ubuntu-vdi.us-east1-c.c.tough-access-431321-g9.internal>
2024-08-08 14:28:32 +0000

    linux vdi

---
+++ linux-vdi/setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo apt update
+sudo apt install --assume-yes wget tasksel
+wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
+sudo apt-get install --assume-yes ./chrome-remote-desktop_current_amd64.deb
+sudo tasksel install ubuntu-desktop
+sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/gnome-session" > /etc/chrome-remote-desktop-session'
+sudo reboot
